c28ecd94cb21132cee9424a07e4d866d961c04a7
Jason Pyke <pykeja@uw.edu>
2022-06-02 13:01:06 -0700

    changes c:ommit

--- backend/routes/api/v1/utils/getBookCoverImageURL.js
+++
@@ -1,7 +0,0 @@
-// gets the book cover url
-// size can be S, M or L
-function getBookCoverImageURL(isbn, size) {
-    return `https://covers.openlibrary.org/b/ISBN/${isbn}-${size}.jpg`;
-}
-
-export default getBookCoverImageURL;
